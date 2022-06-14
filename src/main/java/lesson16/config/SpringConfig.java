c6b875b8f731d576098a280dee69abd562a90eb1
yarmail <yarmail@yandex.ru>
2022-06-14 20:38:34 +0300

    lesson 29 + fix

--- src/main/java/lesson16/config/SpringConfig.java
+++ src/main/java/lesson16/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,8 +17,17 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
+import java.util.Objects;
 
+
+/**
+ * Урок 29
+ * Добавляем Аннотацию (собака)PropertySource
+ * для подключения файла property c данными базы данных
+ * и подключаем Environment для использования
+ * этими свойствами
+ */
+@PropertySource("classpath:database.properties")
 @Configuration
 @ComponentScan ({"lesson16", "lesson21"})
 // Изменения 2 папки - 17, 20, 21
@@ -24,10 +35,12 @@ import java.sql.DriverManager;
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -59,14 +72,18 @@ public class SpringConfig implements WebMvcConfigurer {
      * использовать JdbcTemplate
      * Как я понимаю в этом классе мы описывем нашу базу данных
      * Переносим данные, которые раньше были в PersonDAO
+     * ---
+     * Урок 29 переносим все чувствительные данные
+     * в resource в качестве property-файлов
+     *
      */
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("password");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("user_name"));
+        dataSource.setPassword(environment.getProperty("password"));
         return dataSource;
     }
 
